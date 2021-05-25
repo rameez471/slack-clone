209d6b0894099fb5e6b8049f29cb2bf1e41045c4
rameez471 <yashpathak29@gmail.com>
2021-05-25 21:30:28 +0530

    Header UI

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAE1u6DgiB-WAWoAy9lIIV_2__xhWclPo4",
+    authDomain: "slack-clone-73255.firebaseapp.com",
+    projectId: "slack-clone-73255",
+    storageBucket: "slack-clone-73255.appspot.com",
+    messagingSenderId: "925353565376",
+    appId: "1:925353565376:web:b843c23a65342956a5e125"
+  };
